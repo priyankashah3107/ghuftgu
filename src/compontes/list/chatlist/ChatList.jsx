64bf2a3165c8fbf4bb52767f4f3e1120ce8dffb5
Priyanka <priyankashah3107@gmail.com>
2024-04-16 22:52:22 +0530

    Issue inScroll

--- src/compontes/list/chatlist/ChatList.jsx
+++ src/compontes/list/chatlist/ChatList.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-// import './ChatList.css'
+import './chatList.css'
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false)
   return (
    <>
-    <div className="chatList flex">
+    <div className="chatList ">
       <div className="search flex items-center justify-center p-[20px] gap-[20px]">
         <div className="searchBar flex-1 flex justify-center items-center bg-slate-200 rounded-md gap-2">
           <img src="/search.svg" alt="search" className='size-7' />
@@ -15,6 +15,80 @@ const ChatList = () => {
 '
         onClick={() => setAddMode((prev) => !prev)} />
       </div>
+
+      <div className="item flex justify-start items-center gap-[20px] pb-2 pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+       <div className="item flex justify-start items-center pb-2 gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+        <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+    
+       <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+
+       <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+       <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+       <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+       <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
+       <div className="item flex justify-start  pb-2 items-center gap-[20px] pl-6 mt-4 border-b-[2px] border-b-[#a8a8a8] cursor-pointer">
+          <img src="/avatar.png" alt="avatar" />
+           <div className="texts">
+            <span>Priyanka</span>
+            <p>Hello Universe</p>
+           </div>
+       </div>
+
     </div>
    </>
   )
