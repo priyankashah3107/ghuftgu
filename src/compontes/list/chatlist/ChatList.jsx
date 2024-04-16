21e371040a42044af481ffea30e69e3c950e3bc9
Priyanka <priyankashah3107@gmail.com>
2024-04-16 21:59:31 +0530

    searchBar

--- src/compontes/list/chatlist/ChatList.jsx
+++ src/compontes/list/chatlist/ChatList.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+// import './ChatList.css'
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
   return (
-    <div>ChatList</div>
+   <>
+    <div className="chatList flex">
+      <div className="search flex items-center justify-center p-[20px] gap-[20px]">
+        <div className="searchBar flex-1 flex justify-center items-center bg-slate-200 rounded-md gap-2">
+          <img src="/search.svg" alt="search" className='size-7' />
+          <input type="text" placeholder='Search.......' className='text-black rounded-md bg-transparent 
+           outline-none bg-white flex-1 ' />
+        </div>
+        <img src={addMode ? "/minus.svg" : "/plus.svg"} alt="plus" className='size-8 cursor-pointer bg-slate-200 rounded-md 
+'
+        onClick={() => setAddMode((prev) => !prev)} />
+      </div>
+    </div>
+   </>
   )
 }
 
