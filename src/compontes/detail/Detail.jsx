ebd6976a445b90e37287edb4f0ea60cef4053cbb
Priyanka <priyankashah3107@gmail.com>
2024-04-17 21:29:18 +0530

    fix css error

--- src/compontes/detail/Detail.jsx
+++ src/compontes/detail/Detail.jsx
@@ -11,57 +11,37 @@ const Detail = () => {
           <p className='pb-2 mt-1'>Live before die 🤞</p>
         </div>
       </div>
-
-
+      
       <div className="info">
 
         <div className="option">
           <div className="title">
             <span>Chat Setting</span>
-            <img src="arrowup.svg" alt="arrowup" />
+            <img src="/arrowup.svg" alt="" />
           </div>
         </div>
 
         <div className="option">
           <div className="title">
             <span>Privacy & help</span>
-            <img src="arrowup.svg" alt="arrowdup" />
+            <img src="/arrowup.svg" alt="" />
           </div>
         </div>
 
         <div className="option">
           <div className="title">
             <span>Shared photos</span>
-            <img src="arrowdown.svg" alt="arrowdown" />
+            <img src="/arrowdown.svg" alt="" />
           </div>
-
-          <div className="photos flex flex-row justify-center items-center text-center ">
-            <div className="photoItems ">
-              <img src="nexflix.png" alt="img" className=''/>
-            </div>
-            <span className='p-2 '>photo_2024.png</span>
-            <img src="download.svg" alt="download" className='w-7 h-7  ' />
-          </div>
-          {/* <img src="download.svg" alt="download" className='' /> */}
-          
-         
-
         </div>
 
-
-
-          
         <div className="option">
           <div className="title">
             <span>Shared files</span>
-            <img src="arrowup.svg" alt="arrowdup" />
+            <img src="/arrowup.svg" alt="" />
           </div>
         </div>
-        <div className='button flex flex-col mt-16 gap-4' >
-        <button className="btn btn-error">Block User</button>
-        <button className="btn btn-active btn-primary">Logout</button>
-        </div>
-      
+
 
       </div>
     </div>
