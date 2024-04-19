59fd7b1e95090aff8a5cc4f50c54cbfbe2f70f41
Priyanka <priyankashah3107@gmail.com>
2024-04-19 15:23:12 +0530

    Fix firebase stroage issue

--- src/context/FirebaseAuth.jsx
+++ src/context/FirebaseAuth.jsx
@@ -1,27 +1,35 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { app } from "../../firebase";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged  } from "firebase/auth"
-import { doc, setDoc } from "firebase/firestore"; 
-import { db } from "../../firebase";
-import { set } from "firebase/database";
+import { createContext, useContext, useEffect, useState } from 'react';
+import { app } from '../../firebase';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { set } from 'firebase/database';
 
-const FireBaseContext = createContext(null)
+const FireBaseContext = createContext(null);
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export const useFirbase = () =>  useContext(FireBaseContext);
+export const useFirbase = () => useContext(FireBaseContext);
 
-
-const createUserEmailPass = async(email, password, username) => {
-   createUserWithEmailAndPassword(auth, email, password, username)
-  
-  
-}
+const createUserEmailPass = async (email, password, username) => {
+  const result = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password,
+    username
+  );
+  return result;
+};
 
 // SingIn
 const SignWithEmailPass = (email, password) => {
-  return signInWithEmailAndPassword(auth,email, password)
-}
+  return signInWithEmailAndPassword(auth, email, password);
+};
 
 // useEffect(() => {
 //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -31,19 +39,14 @@ const SignWithEmailPass = (email, password) => {
 //   return () => unsubscribe();
 // }, []);
 
-
-
-const FirebaseAuth = ({children}) => {
-
-
-
-
+const FirebaseAuth = ({ children }) => {
   return (
-    <FireBaseContext.Provider value={{createUserEmailPass, SignWithEmailPass}}>
+    <FireBaseContext.Provider
+      value={{ createUserEmailPass, SignWithEmailPass }}
+    >
       {children}
-      
     </FireBaseContext.Provider>
-  )
-}
+  );
+};
 
-export default FirebaseAuth
+export default FirebaseAuth;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,143 +1,127 @@
-import React, { useState } from 'react'
-import { useFirbase } from '../context/FirebaseAuth'
-import { doc, setDoc } from "firebase/firestore"; 
+
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import { db } from '../../firebase';
-// import { createUserWithEmailAndPassword } from 'firebase/auth';
-// import { app } from '../../firebase';
+import { app } from '../../firebase';
+import { useFirbase } from '../context/FirebaseAuth';
+const auth = getAuth(app);
+
 const RegisterPage = () => {
-  
-  // const [email, setEmail] = useState('')
-  // const[password, setPassword] = useState('')
-  // const [username, setuserName] = useState('')
-   
-  // console.log(username)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
-
-  const[avatar, setAvatar] = useState({
-     file: null,
-     url: ""
-  })
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: '',
+  });
 
   const handleAvatar = (e) => {
-     
-    if(e.target.files[0]) {
+    if (e.target.files[0]) {
       setAvatar({
         file: e.target.files[0],
-        url: URL.createObjectURL(e.target.files[0])
-      })
+        url: URL.createObjectURL(e.target.files[0]),
+      });
     }
-  }
+  };
 
+  const firebase = useFirbase();
+  console.log(firebase);
 
-  const firebase = useFirbase()
-  console.log(firebase)
-
-  // const handleSubmit = async(e) => {
-  //    e.preventDefault()
-  //    if(!username || !email || !password) {
-  //     alert("Please Fill all the feild")
-  //     return
-  //    }
-
-
-
-  //   try {
-  //     const result = await firebase.createUserEmailPass(email, password, username);
-  //     // const result = await createUserWithEmailAndPassword(firebase.auth, email, password);
-  //     console.log('Registration successful:', result);
-      
-  //     // await setDoc(doc(db, "users", result.user.uid), {
-  //     //   username,
-  //     //   email,
-  //     //   id: result.user.uid,
-  //     //   blocked: []
-        
-  //     // });
-       
-  //     // await setDoc(doc(db, "userchats", result.user.uid), {
-  //     //   chats: [],
-        
-  //     // });
-       
-  //     toast.success("Account Created! Now you can login 😉")
-  //     alert('Successfully registered!');
-      
-  //   } catch (error) {
-  //     alert('Registration failed. Please try again.');
-  //       console.log("Error occur during Registration", error)
-  //   }
-
-  //   //  console.log("signUp to the User")
-  //   // const result =  await firebase.createUserEmailPass(email, password)
-  //   // console.log("Registration Successfully", result)
-  //   //  alert("successfully registred")
-
-  // }
-
-
-
-   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target)
-
-    const {username, email, password} = Object.fromEntries(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!name || !email || !password) {
+      alert('Please Fill all the feild');
+      return;
+    }
 
     try {
-      const res = await createUserWithEmailAndPassword(auth, email, password)
-
-      await setDoc(doc(db, "users", res.user.uid), {
-        username,
-       email,
-        id: res.user.id,
-        blocked: []
+      // const result = await createUserWithEmailAndPassword(
+      //   auth,
+      //   email,
+      //   password
+      // );
+      const result = await firebase.createUserEmailPass(email, password, name);
+      // const result = await createUserEmailPass(email, password);
+      console.log('Registration successful:', result);
+      alert('Successfully registered!');
+      await setDoc(doc(db, 'users', result.user.uid), {
+        name,
+        email,
+        id: result.user.uid,
+        blocked: [],
       });
-      
-      await setDoc(doc(db, "userchats", res.user.uid), {
-        chats: []
+
+      await setDoc(doc(db, 'userchats', result.user.uid), {
+        chats: [],
       });
-
-      toast.success("Account Created!")
-
-    } catch (err) {
-      
-      toast.error(err.message)
-      console.log(err)
+    } catch (error) {
+      alert('Registration failed. Please try again.');
+      console.log('Error occur during Registration', error);
     }
- }
+
+    //  console.log("signUp to the User")
+    // const result =  await firebase.createUserEmailPass(email, password)
+    // console.log("Registration Successfully", result)
+    //  alert("successfully registred")
+  };
 
   return (
-    <div className='text-black '>
-    <form onSubmit={handleSubmit}>
-     
-     <h4 className="text-white text-[40px] font-normal font-['Lucida Calligraphy']  "> Create an Account</h4>
-      <div className='flex flex-row items-center gap-4 '>
-      {/* <div className='w-16 h-16 bg-zinc-800 rounded-lg text-4xl text-center items-center justify-center pt-3 text-white mt-2'>A  </div> */}
-          
-      <label htmlFor="file" className='text-white font-bold mt-2'>
-        <img src={ avatar.url || "avatar.png"} alt="avatar" className='size-[50px] cover border' />
-       </label>  
-       <input type='file' id='file' className='underline text-white w-24'
-       onChange={handleAvatar}/>  
+    <div className="text-black ">
+      <form onSubmit={handleSubmit}>
+        <h4 className="text-white text-[40px] font-normal font-['Lucida Calligraphy']  ">
+          {' '}
+          Create an Account
+        </h4>
+        <div className="flex flex-row items-center gap-4 ">
+          {/* <div className='w-16 h-16 bg-zinc-800 rounded-lg text-4xl text-center items-center justify-center pt-3 text-white mt-2'>A  </div> */}
 
-      </div>
-     <div className="flex flex-col gap-8 mt-10">
+          <label htmlFor="file" className="text-white font-bold mt-2">
+            <img
+              src={avatar.url || 'avatar.png'}
+              alt="avatar"
+              className="size-[50px] cover border"
+            />
+          </label>
+          <input
+            type="file"
+            id="file"
+            className="underline text-white w-24"
+            onChange={handleAvatar}
+          />
+        </div>
+        <div className="flex flex-col gap-8 mt-10">
+          <input
+            type="text"
+            placeholder="Username"
+            className="input input-bordered w-full max-w-xs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
 
-     <input type="text" name='username' placeholder="Username" className="input input-bordered w-full max-w-xs"
-     />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="input input-bordered w-full max-w-xs"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            className="input input-bordered w-full max-w-xs"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn btn-primary w-full max-w-xs" type="submit">
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-     <input type="email" name='email' placeholder="Enter Email" className="input input-bordered w-full max-w-xs" 
-     />
-     <input type="password" name='password' placeholder="Enter password" className="input input-bordered w-full max-w-xs"
-    
-   
-     />
-     <button className="btn btn-primary w-full max-w-xs" type='submit'>Sign Up</button>
-
-     </div>
-    </form>
- </div>
-  )
-}
-
-export default RegisterPage
+export default RegisterPage;
