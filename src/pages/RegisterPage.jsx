e2b521a5ec577cb445b79fb302bbd916529afe46
Priyanka <priyankashah3107@gmail.com>
2024-04-19 14:29:58 +0530

    err

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -7,9 +7,9 @@ import { db } from '../../firebase';
 // import { app } from '../../firebase';
 const RegisterPage = () => {
   
-  const [email, setEmail] = useState('')
-  const[password, setPassword] = useState('')
-  const [username, setuserName] = useState('')
+  // const [email, setEmail] = useState('')
+  // const[password, setPassword] = useState('')
+  // const [username, setuserName] = useState('')
    
   // console.log(username)
 
@@ -33,47 +33,78 @@ const RegisterPage = () => {
   const firebase = useFirbase()
   console.log(firebase)
 
-  const handleSubmit = async(e) => {
-     e.preventDefault()
-     if(!username || !email || !password) {
-      alert("Please Fill all the feild")
-      return
-     }
+  // const handleSubmit = async(e) => {
+  //    e.preventDefault()
+  //    if(!username || !email || !password) {
+  //     alert("Please Fill all the feild")
+  //     return
+  //    }
 
 
+
+  //   try {
+  //     const result = await firebase.createUserEmailPass(email, password, username);
+  //     // const result = await createUserWithEmailAndPassword(firebase.auth, email, password);
+  //     console.log('Registration successful:', result);
+      
+  //     // await setDoc(doc(db, "users", result.user.uid), {
+  //     //   username,
+  //     //   email,
+  //     //   id: result.user.uid,
+  //     //   blocked: []
+        
+  //     // });
+       
+  //     // await setDoc(doc(db, "userchats", result.user.uid), {
+  //     //   chats: [],
+        
+  //     // });
+       
+  //     toast.success("Account Created! Now you can login 😉")
+  //     alert('Successfully registered!');
+      
+  //   } catch (error) {
+  //     alert('Registration failed. Please try again.');
+  //       console.log("Error occur during Registration", error)
+  //   }
+
+  //   //  console.log("signUp to the User")
+  //   // const result =  await firebase.createUserEmailPass(email, password)
+  //   // console.log("Registration Successfully", result)
+  //   //  alert("successfully registred")
+
+  // }
+
+
+
+   const handleSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+
+    const {username, email, password} = Object.fromEntries(formData);
 
     try {
-      const result = await firebase.createUserEmailPass(email, password, username);
-      // const result = await createUserWithEmailAndPassword(firebase.auth, email, password);
-      console.log('Registration successful:', result);
+      const res = await createUserWithEmailAndPassword(auth, email, password)
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+       email,
+        id: res.user.id,
+        blocked: []
+      });
       
-      // await setDoc(doc(db, "users", result.user.uid), {
-      //   username,
-      //   email,
-      //   id: result.user.uid,
-      //   blocked: []
-        
-      // });
-       
-      // await setDoc(doc(db, "userchats", result.user.uid), {
-      //   chats: [],
-        
-      // });
-       
-      toast.success("Account Created! Now you can login 😉")
-      alert('Successfully registered!');
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: []
+      });
+
+      toast.success("Account Created!")
+
+    } catch (err) {
       
-    } catch (error) {
-      alert('Registration failed. Please try again.');
-        console.log("Error occur during Registration", error)
+      toast.error(err.message)
+      console.log(err)
     }
-
-    //  console.log("signUp to the User")
-    // const result =  await firebase.createUserEmailPass(email, password)
-    // console.log("Registration Successfully", result)
-    //  alert("successfully registred")
-
-  }
+ }
 
   return (
     <div className='text-black '>
@@ -93,13 +124,13 @@ const RegisterPage = () => {
      <div className="flex flex-col gap-8 mt-10">
 
      <input type="text" name='username' placeholder="Username" className="input input-bordered w-full max-w-xs"
-     value={username} onChange={(e) => setuserName(e.target.value)} />
+     />
 
      <input type="email" name='email' placeholder="Enter Email" className="input input-bordered w-full max-w-xs" 
-     value={email} onChange={(e) => setEmail(e.target.value)}/>
+     />
      <input type="password" name='password' placeholder="Enter password" className="input input-bordered w-full max-w-xs"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
+    
+   
      />
      <button className="btn btn-primary w-full max-w-xs" type='submit'>Sign Up</button>
 
