7b89eeefb10962480529c2c8f9e260755afcb978
Priyanka <priyankashah3107@gmail.com>
2024-05-02 10:37:05 +0530

    little modification

--- src/compontes/list/chatlist/adduser/AddUser.jsx
+++ src/compontes/list/chatlist/adduser/AddUser.jsx
@@ -4,30 +4,42 @@ import { collection, getDoc, getDocs, query, setDoc, where } from "firebase/fire
 import { db } from '../../../../../firebase';
 const AddUser = () => {
   const [name, setName] = useState('')
-  console.log(name)
+  const [userData, setUserData] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  // console.log(name)
 
 
   const handleSearch = async(ev) => {
     ev.preventDefault();
+     
+    setIsLoading(true)
+    setErrorMessage(null)
 
-    const formData = new FormData(ev.target)
-    const username = formData.get("username")
+    // const formData = new FormData(ev.target)
+    // const username = formData.get("username")
 
      try {
 
         const userRef = collection(db, "users");
       // Create a query against the collection.
-          const q = query(userRef, where("username", "==", username));
+          const q = query(userRef, where("username", "==", name));
           const querySnapShot =  await getDocs(q)
       
-          if(!querySnapShot.empty) {
-            setName(querySnapShot.docs[0].data())
+          if(querySnapShot.empty) {
+            setUserData(null)
+            setErrorMessage("No User found for the entered username");
           } else {
-            setName('')
+            const userDoc = querySnapShot.docs[0]
+            setUserData(userDoc.data())
           }
       
      } catch (error) {
-       console.log(error)
+       console.log("Error While Fetching a user", error)
+       setErrorMessage("An Error Occured While Searching for the user");
+     } finally{
+      setIsLoading(false)
      }
  
   }
@@ -38,24 +50,26 @@ const AddUser = () => {
 
       <form onSubmit={handleSearch}>
          <input type="text" placeholder='Search' name='username' className='text-black' 
-         
+         value={name} onChange={(e) => setName(e.target.value)}
          />
          
-         <button className="btn btn-primary">Search</button>
+         <button className="btn btn-primary" disabled={isLoading}>{isLoading ? "Searching" : "Search"}
+         </button>
       </form>
 
-      
-         { name && <div className="user">
+       {errorMessage && <p className='message error'>{errorMessage}</p>}
+
+         { userData && (<div className="user">
             <div className="detail">
-              <img src= { name.avatar || "avatar.png"} alt="avatar" />
+              <img src= { userData.avatar || "avatar.png"} alt="avatar" />
               {/* <span className='text-xl font-bold'>Priyanka</span> */}
-              <span className='text-xl font-bold'>{name.username}</span>
+              <span className='text-xl font-bold'>{userData.username}</span>
             </div>
             <button className="btn btn-accent">Add User</button>
           </div>
-      }
+      )}
 
-        {/* {!user && <p className="message">No user found for the entered username.</p>} */}
+        
 
 
      </div>
@@ -64,13 +78,6 @@ const AddUser = () => {
 }
 
 export default AddUser
-
-
-
-
-
-
-
 
 
 
